cb363a6b4792ec54e4f9131676f9d990fac77da5
Flames <vibe-coder@flames.com>
2025-11-18 13:38:44 +0000

    Update project prj_t5q752y0 - 2025-11-18T13:38:44.177Z

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className="text-center mb-8">
       <h1 className="text-4xl sm:text-5xl font-extrabold tracking-tight text-yellow-200 drop-shadow-[0_0_25px_rgba(255,255,255,0.25)]">
-        Se Hoki apa sih lu?
+        Tes Hoki Lu Disini
       </h1>
       <p className="mt-3 text-red-100/90 text-sm sm:text-base">
         Tekan tombol spin. Jika tiga ikon sama (dan bukan ZONK), kamu menang!
